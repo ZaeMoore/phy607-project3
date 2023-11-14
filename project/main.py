1845f8ef49c1d46d762adf0202fa62e98828c239
Zae Moore <emoore06@syr.edu>
2023-11-14 13:20:18 -0500

    Updated

--- project/main.py
+++ project/main.py
@@ -5,3 +5,25 @@ Main script for project three, Markov Chain Monte Carlo simulation for phy 607
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.random import uniform
+import tqdm
+
+#Import data
+
+def model(x):
+    return 1
+
+def post(x):
+    return likelihood(x) * prior(x)
+
+def prior(x):
+    return 1
+
+def proposal(x):
+    return np.random.normal() + x
+
+def likelihood(m):
+    return 1 
+    #Just a Gaussian normal for the noise
+
+#Params: 
