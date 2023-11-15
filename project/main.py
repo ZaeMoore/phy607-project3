6378553715c8b3d2bfa8e8eae9482dea142ce499
Zae Moore <emoore06@syr.edu>
2023-11-15 14:54:40 -0500

    Updated structure to reflect some of the steps

--- project/main.py
+++ project/main.py
@@ -10,6 +10,19 @@ import tqdm
 
 #Import data
 
+def data(N):
+    return 1
+    #Return an NxN array of spins
+
+def new_array(s):
+    return 1
+    #Given the old array, pick a random spin entry to flip and return the new array
+
+def dH(s):
+    return new_array(s)
+    #Find new array s', calculate the difference between H(s) and H(s')
+    #This will be used to determine if the spin flips
+
 def model(x):
     return 1
 
