adf41fa4c556f5fe79866be44140c7b04d725f72
Zae Moore <emoore06@syr.edu>
2023-11-27 16:36:45 -0500

    Pymc3 practice works

--- project/main_old.py
+++ project/main_old.py
@@ -7,7 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import uniform
 import tqdm
-import emcee
+import pymc3 as pm
+import theano
+import theano.tensor as tt
 
 def data(N):
     """Generate initial random spin data in a lattice structure
@@ -103,19 +105,43 @@ def calculate_energy(lattice, J):
             energy -= J * spin * neighbors
     return energy / 2  # Each pair counted twice
 
-N = 100
-num_steps = 1000
+N = 32
+num_steps = 100
 lattice = data(N)
 beta = 0.4
 J = 1
-order = 8 #Might change this later
 
 finallattice, magnetization, energy = mcmc(lattice, beta, J, num_steps)
 
-#Now do the emcee method here and compare
-#Reference data_post.py from class
-#Create the emcee sampler
-sampler = emcee.EnsembleSampler(500, order, delta_energy)
+
+#pymc3
+def get_H(spins):
+    H = - (
+        tt.roll(spins, 1, axis=1) * spins +
+        tt.roll(spins, 1, axis=0) * spins +
+        tt.roll(spins, -1, axis=1) * spins +
+        tt.roll(spins, -1, axis=0) * spins
+    )
+    return H
+
+def to_spins(lattice):
+    return 2 * lattice - 1
+
+def mc3_approach(beta, N, num_steps=10):
+    shape = (N, N)
+    x0 = np.random.randint(2, size=shape)
+    with pm.Model() as model:
+        x = pm.Bernoulli('x', 0.5, shape=shape, testval=x0)
+        magnetization = pm.Potential('m', -get_H(to_spins(x)) * beta)
+        scaling = .0006
+        mul = int(N * N * 1.75)
+        step = pm.BinaryMetropolis([x], scaling=scaling)
+        trace = pm.sample(num_steps * mul * 5, step=step, chains=1, tune=False)
+    lattice = 2 * trace[-1]['x'] - 1
+    return lattice, trace
+
+mc3_lattice, trace = mc3_approach(beta, N)
+
 
 plt.figure()
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
@@ -125,6 +151,14 @@ im = plt.imshow(finallattice, cmap="magma")
 plt.colorbar(im)
 plt.show()
 
+
+plt.figure()
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+plt.title("Final Spin Lattice pymc3")
+im = plt.imshow(mc3_lattice, cmap="magma")
+plt.colorbar(im)
+plt.show()
 plt.figure()
 plt.plot(magnetization)
 plt.title("Magnetization")
